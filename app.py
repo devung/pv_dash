a7be2a1e400e29d4e7a09d16cd0749d215dc7297
Lim Ung <limung.dev@gmail.com>
2022-09-14 21:03:50 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import pandas as pd                 # pip install pandas openpyxl
-import plotly.express as px         # pip install plotly-express
+import pandas as pd
+import plotly.express as px
 import plotly.graph_objects as go
-import streamlit as st              # pip install streamlit
+import streamlit as st
 import calendar
 
 st.set_page_config(
